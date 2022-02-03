1b813b727c6f29b2eb55d98b01c2d93afdfc2dfc
Felipe Garcia <felipegarcia1402@gmail.com>
2022-02-03 02:00:37 -0300

    implement "get_model"

--- src/sudoku_solver.cpp
+++ src/sudoku_solver.cpp
@@ -2,13 +2,33 @@
 
 namespace css
 {
-    bool sudoku_solver::solve() noexcept
+    board sudoku_solver::get_model()
     {
-        return false;
-    }
+        board result{ nrows, board_row{ ncols } };
 
-    board sudoku_solver::get_model() noexcept
-    {
-        return {};
+        for (uint32_t row{}; row < nrows; ++row)
+        {
+            for (uint32_t col{}; col < ncols; ++col)
+            {
+                uint32_t nfound{};
+
+                for (uint32_t val{}; val < nvals; ++val)
+                {
+                    auto pos = make_board_pos(row, col, val);
+                    auto mval = m_solver.modelValue(make_unique_var(dim, pos));
+                    
+                    if (mval.isTrue())
+                    {
+                        ++nfound;
+                        result[row][col] = val + 1;
+                    }
+                }
+
+                if (nfound != 1)
+                    throw std::runtime_error{ "[-] Rule Break: The solver assigned one position with more than one value" };
+            }
+        }
+
+        return result;
     } 
 }
